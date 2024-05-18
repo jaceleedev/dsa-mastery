d1640b3ddb7cead182bb3e00b569db99602e4e4f
jaceleedev <jaceleedev@gmail.com>
2024-05-19 01:17:47 +0900

    feat: :sparkles: implement append

--- data-structures/linked-list/singly-linked-list.js
+++ data-structures/linked-list/singly-linked-list.js
@@ -10,4 +10,23 @@ class SinglyLinkedList {
     this.head = null;
     this.size = 0;
   }
+
+  // 리스트의 끝에 노드 추가
+  append(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while (current.next !== null) {
+        current = current.next;
+      }
+
+      current.next = newNode;
+    }
+
+    ++this.size;
+  }
 }

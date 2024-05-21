cd92b3086f6c109560f1b5d26833ea686af6fc1c
jaceleedev <jaceleedev@gmail.com>
2024-05-21 10:57:37 +0900

    feat: :sparkles: implement append

--- data-structures/linked-list/doubly-linked-list.js
+++ data-structures/linked-list/doubly-linked-list.js
@@ -12,4 +12,24 @@ class DoublyLinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  /**
+   * 리스트의 끝에 노드를 추가합니다.
+   * 시간 복잡도: O(1)
+   * @param {any} data - 추가할 데이터
+   */
+  append(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+
+    ++this.size;
+  }
 }

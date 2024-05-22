2a723054dfb84ee140cb1415d37454735fe98018
jaceleedev <jaceleedev@gmail.com>
2024-05-22 22:39:03 +0900

    feat: :sparkles: implement removeAt

--- data-structures/linked-list/circular-singly-linked-list.js
+++ data-structures/linked-list/circular-singly-linked-list.js
@@ -51,4 +51,52 @@ class CircularSinglyLinkedList {
 
     ++this.size;
   }
+
+  /**
+   * 특정 위치의 노드를 제거하고 데이터를 반환합니다.
+   * 시간 복잡도: O(n)
+   * @param {number} index - 제거할 노드의 위치
+   * @returns {any} 제거된 노드의 데이터
+   */
+  removeAt(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    let removedData = null;
+
+    if (index === 0) {
+      removedData = this.head.data;
+
+      if (this.size === 1) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+        this.tail.next = this.head;
+      }
+    } else {
+      let current = this.head;
+      let previous = null;
+      let count = 0;
+
+      while (count < index) {
+        previous = current;
+        current = current.next;
+        ++count;
+      }
+
+      removedData = current.data;
+      previous.next = current.next;
+
+      if (index === this.size - 1) {
+        this.tail = previous;
+        this.tail.next = this.head;
+      }
+    }
+
+    --this.size;
+
+    return removedData;
+  }
 }

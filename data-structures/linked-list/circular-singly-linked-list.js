d665c668095197e550a8f8d6cd3b00837c4a83ae
jaceleedev <jaceleedev@gmail.com>
2024-05-22 22:17:46 +0900

    feat: :sparkles: implement append

--- data-structures/linked-list/circular-singly-linked-list.js
+++ data-structures/linked-list/circular-singly-linked-list.js
@@ -11,4 +11,24 @@ class CircularSinglyLinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  /**
+   * 리스트의 끝에 노드를 추가합니다.
+   * 시간 복잡도: O(1)
+   * @param {any} data - 추가할 데이터
+   */
+  append(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+      this.tail.next = this.head;
+    }
+
+    ++this.size;
+  }
 }

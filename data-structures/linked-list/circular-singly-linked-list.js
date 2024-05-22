c22902ecb22f04505e8571ea05024ab8c5a6505c
jaceleedev <jaceleedev@gmail.com>
2024-05-22 22:19:03 +0900

    feat: :sparkles: implement prepend

--- data-structures/linked-list/circular-singly-linked-list.js
+++ data-structures/linked-list/circular-singly-linked-list.js
@@ -31,4 +31,24 @@ class CircularSinglyLinkedList {
 
     ++this.size;
   }
+
+  /**
+   * 리스트의 시작 부분에 노드를 추가합니다.
+   * 시간 복잡도: O(1)
+   * @param {any} data - 추가할 데이터
+   */
+  prepend(data) {
+    const newNode = new Node(data);
+
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      this.tail.next = this.head;
+    }
+
+    ++this.size;
+  }
 }

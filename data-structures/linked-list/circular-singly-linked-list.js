f55fe59b41bca4c89cb2e3de022641ad6e934c23
jaceleedev <jaceleedev@gmail.com>
2024-05-26 13:51:39 +0900

    feat: :sparkles: implement toArray

--- data-structures/linked-list/circular-singly-linked-list.js
+++ data-structures/linked-list/circular-singly-linked-list.js
@@ -374,4 +374,23 @@ class CircularSinglyLinkedList {
     this.tail = this.head;
     this.head = previous;
   }
+
+  /**
+   * 리스트를 배열로 변환합니다.
+   * 시간 복잡도: O(n)
+   * @returns {any[]} 리스트의 요소를 포함하는 배열
+   */
+  toArray() {
+    const array = [];
+    let current = this.head;
+    let nodesChecked = 0;
+
+    while (nodesChecked < this.size) {
+      array.push(current.data);
+      current = current.next;
+      ++nodesChecked;
+    }
+
+    return array;
+  }
 }

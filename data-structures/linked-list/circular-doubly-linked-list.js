29f8857ed7ec80adb699eb46989bd65fdda1427a
jaceleedev <jaceleedev@gmail.com>
2024-05-27 12:56:14 +0900

    feat: :sparkles: implement append

--- data-structures/linked-list/circular-doubly-linked-list.js
+++ data-structures/linked-list/circular-doubly-linked-list.js
@@ -12,4 +12,32 @@ class CircularDoublyLinkedList {
     this.tail = null;
     this.size = 0;
   }
+
+  /**
+   * 리스트의 끝에 노드를 추가합니다.
+   * 시간 복잡도: O(1)
+   * @param {any} data - 추가할 데이터
+   */
+  append(data) {
+    const newNode = new Node(data);
+
+    // 리스트가 비어있는 경우
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = newNode;
+      newNode.prev = newNode;
+
+    }
+    // 리스트에 노드가 있는 경우
+    else {
+      newNode.prev = this.tail;
+      newNode.next = this.head;
+      this.tail.next = newNode;
+      this.head.prev = newNode;
+      this.tail = newNode;
+    }
+
+    ++this.size;
+  }
 }

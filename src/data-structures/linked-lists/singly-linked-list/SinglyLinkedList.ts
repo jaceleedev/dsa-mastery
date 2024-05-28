f452a7b8ef6ff988fc5e0d2d739489fac43c0baa
jaceleedev <jaceleedev@gmail.com>
2024-05-28 20:48:25 +0900

    feat: :sparkles: implement insertAt in SinglyLinkedList

--- src/data-structures/linked-lists/singly-linked-list/SinglyLinkedList.ts
+++ src/data-structures/linked-lists/singly-linked-list/SinglyLinkedList.ts
@@ -16,4 +16,32 @@ class SinglyLinkedList<T> {
     this.head = null;
     this.size = 0;
   }
+
+  insertAt(index: number, data: T): void {
+    if (index < 0 || index > this.size) {
+      throw new RangeError('Index out of bounds');
+    }
+
+    const newNode = new ListNode(data);
+
+    if (index === 0) {
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      let previous = null;
+      let count = 0;
+
+      while (count < index) {
+        current = current!.next;
+        previous = current;
+        ++count;
+      }
+
+      newNode.next = current;
+      previous!.next = newNode;
+    }
+
+    ++this.size;
+  }
 }

6bc5604cb3da00de9d499d5e6cb3c45368371d6d
jaceleedev <jaceleedev@gmail.com>
2024-05-28 20:46:48 +0900

    feat: :sparkles: implement constructor in SinglyLinkedList

--- src/data-structures/linked-lists/singly-linked-list/SinglyLinkedList.ts
+++ src/data-structures/linked-lists/singly-linked-list/SinglyLinkedList.ts
@@ -7,3 +7,13 @@ class ListNode<T> {
     this.next = null;
   }
 }
+
+class SinglyLinkedList<T> {
+  head: ListNode<T> | null;
+  size: number;
+
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+}

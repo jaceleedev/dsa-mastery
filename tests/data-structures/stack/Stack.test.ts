5aed923dc378845776fa6cb99e1c46241079d5a9
jaceleedev <jaceleedev@gmail.com>
2024-06-01 18:32:12 +0900

    test: :white_check_mark: push elements to Stack

--- tests/data-structures/stack/Stack.test.ts
+++ tests/data-structures/stack/Stack.test.ts
@@ -11,4 +11,12 @@ describe('스택', () => {
     expect(stack.isEmpty()).toBe(true);
     expect(stack.length()).toBe(0);
   });
+
+  test('요소를 push할 수 있어야 합니다.', () => {
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    expect(stack.length()).toBe(3);
+    expect(stack.toArray()).toEqual([1, 2, 3]);
+  });
 });

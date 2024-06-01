891be4a5a569d1bd8ec3ff165f8da2d34ae4c524
jaceleedev <jaceleedev@gmail.com>
2024-06-01 18:58:19 +0900

    test: :recycle: remove periods from test descriptions for consistency

--- tests/data-structures/stack/Stack.test.ts
+++ tests/data-structures/stack/Stack.test.ts
@@ -7,12 +7,12 @@ describe('스택', () => {
     stack = new Stack<number>();
   });
 
-  test('초기에는 비어있어야 합니다.', () => {
+  test('초기에는 비어있어야 합니다', () => {
     expect(stack.isEmpty()).toBe(true);
     expect(stack.length()).toBe(0);
   });
 
-  test('요소를 push할 수 있어야 합니다.', () => {
+  test('요소를 push할 수 있어야 합니다', () => {
     stack.push(1);
     stack.push(2);
     stack.push(3);
@@ -20,24 +20,24 @@ describe('스택', () => {
     expect(stack.toArray()).toEqual([1, 2, 3]);
   });
 
-  test('요소를 pop할 수 있어야 합니다.', () => {
+  test('요소를 pop할 수 있어야 합니다', () => {
     stack.push(1);
     stack.push(2);
     expect(stack.pop()).toBe(2);
     expect(stack.pop()).toBe(1);
   });
 
-  test('빈 스택에서 pop할 때, 에러가 발생되어야 합니다.', () => {
+  test('빈 스택에서 pop할 때, 에러가 발생되어야 합니다', () => {
     expect(() => stack.pop()).toThrow(Error);
   });
 
-  test('스택의 맨 위 요소를 확인할 때, 해당 요소가 반환되어야 합니다.', () => {
+  test('스택의 맨 위 요소를 확인할 때, 해당 요소가 반환되어야 합니다', () => {
     stack.push(1);
     stack.push(2);
     expect(stack.peek()).toBe(2);
   });
 
-  test('빈 스택에서 peek할 때, 에러가 발생되어야 합니다.', () => {
+  test('빈 스택에서 peek할 때, 에러가 발생되어야 합니다', () => {
     expect(() => stack.peek()).toThrow(Error);
   });
 

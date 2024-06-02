29dc19ca7d4f44f3b2d70ed92e85129e3644062b
jaceleedev <jaceleedev@gmail.com>
2024-06-02 17:33:00 +0900

    test: :white_check_mark: enqueue elements to Queue

--- tests/data-structures/queue/queue.test.ts
+++ tests/data-structures/queue/queue.test.ts
@@ -11,4 +11,12 @@ describe('큐', () => {
     expect(queue.isEmpty()).toBe(true);
     expect(queue.length()).toBe(0);
   });
+
+  test('요소를 enqueue할 수 있어야 합니다.', () => {
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    expect(queue.length()).toBe(3);
+    expect(queue.toArray()).toEqual([1, 2, 3]);
+  });
 });

7751cdbfece02d80c1f7ed4f5702aa847cfb0ea0
jaceleedev <jaceleedev@gmail.com>
2024-06-03 20:07:47 +0900

    feat: :sparkles: implement insert in BST

--- src/data-structures/tree/binary-search-tree/BinarySearchTree.ts
+++ src/data-structures/tree/binary-search-tree/BinarySearchTree.ts
@@ -16,4 +16,31 @@ class BinarySearchTree<T> {
   constructor() {
     this.root = null;
   }
+
+  insert(value: T): void {
+    const newNode = new TreeNode(value);
+
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+
+    this.insertNode(this.root, newNode);
+  }
+
+  insertNode(node: TreeNode<T>, newNode: TreeNode<T>): void {
+    if (newNode.value < node.value) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
 }

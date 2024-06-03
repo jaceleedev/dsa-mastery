363e28a1a7f768e2da8d2107a994d7b8217d4402
jaceleedev <jaceleedev@gmail.com>
2024-06-03 19:41:17 +0900

    feat: :sparkles: add TreeNode for BST

---
+++ src/data-structures/tree/binary-search-tree/BinarySearchTree.ts
@@ -0,0 +1,11 @@
+class TreeNode<T> {
+  left: TreeNode<T> | null;
+  right: TreeNode<T> | null;
+  value: T;
+
+  constructor(value: T) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}

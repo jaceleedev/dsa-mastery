21564ff28691e665083436660ecc5b66601aede9
jaceleedev <jaceleedev@gmail.com>
2024-06-06 15:54:37 +0900

    test: :white_check_mark: insert elements to BST

---
+++ tests/data-structures/tree/binary-search-tree/BinarySearchTree.test.ts
@@ -0,0 +1,20 @@
+import { BinarySearchTree } from '../../../../src/data-structures/tree/binary-search-tree/BinarySearchTree';
+
+describe('이진 탐색 트리', () => {
+  let bst: BinarySearchTree<number>;
+
+  beforeEach(() => {
+    bst = new BinarySearchTree<number>();
+    bst.insert(10);
+    bst.insert(5);
+    bst.insert(15);
+    bst.insert(3);
+    bst.insert(7);
+    bst.insert(12);
+    bst.insert(18);
+  });
+
+  test('요소를 삽입할 수 있어야 합니다', () => {
+    expect(bst.inOrderTraversal()).toEqual([3, 5, 7, 10, 12, 15, 18]);
+  });
+});

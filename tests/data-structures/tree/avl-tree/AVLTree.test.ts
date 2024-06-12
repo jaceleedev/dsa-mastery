242639fe7196eb031040544cc0217da7817a9b8f
jaceleedev <jaceleedev@gmail.com>
2024-06-12 22:47:01 +0900

    test: :white_check_mark: insert elements to AVL Tree

---
+++ tests/data-structures/tree/avl-tree/AVLTree.test.ts
@@ -0,0 +1,20 @@
+import { AVLTree } from '../../../../src/data-structures/tree/avl-tree/AVLTree';
+
+describe('AVL 트리', () => {
+  let avl: AVLTree<number>;
+
+  beforeEach(() => {
+    avl = new AVLTree<number>();
+    avl.insert(10);
+    avl.insert(5);
+    avl.insert(15);
+    avl.insert(3);
+    avl.insert(7);
+    avl.insert(12);
+    avl.insert(18);
+  });
+
+  test('요소를 삽입할 수 있어야 합니다', () => {
+    expect(avl.inOrderTraversal()).toEqual([3, 5, 7, 10, 12, 15, 18]);
+  });
+});

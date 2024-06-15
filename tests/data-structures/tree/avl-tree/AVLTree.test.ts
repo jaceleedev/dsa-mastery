d56393703d5434b94b4dbd4f1e8698b1ee69f9b5
jaceleedev <jaceleedev@gmail.com>
2024-06-15 22:46:35 +0900

    test: :white_check_mark: rotate Right-Left for Right-Left case in AVL Tree

--- tests/data-structures/tree/avl-tree/AVLTree.test.ts
+++ tests/data-structures/tree/avl-tree/AVLTree.test.ts
@@ -108,4 +108,13 @@ describe('AVL 트리', () => {
     expect(avlLL.inOrderTraversal()).toEqual([10, 20, 30]);
     expect(avlLL.levelOrderTraversal()).toEqual([20, 10, 30]);
   });
+
+  test('Right-Left 케이스에서, Right-Left 회전을 해야 합니다', () => {
+    const avlLL = new AVLTree<number>();
+    avlLL.insert(10);
+    avlLL.insert(30);
+    avlLL.insert(20);
+    expect(avlLL.inOrderTraversal()).toEqual([10, 20, 30]);
+    expect(avlLL.levelOrderTraversal()).toEqual([20, 10, 30]);
+  });
 });

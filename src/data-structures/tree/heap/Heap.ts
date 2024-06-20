452c63ca02b1196ef241d5b3b15f91e70e21a6a4
jaceleedev <jaceleedev@gmail.com>
2024-06-20 09:34:51 +0900

    feat: :sparkles: implement poll in MinHeap

--- src/data-structures/tree/heap/Heap.ts
+++ src/data-structures/tree/heap/Heap.ts
@@ -13,4 +13,24 @@ export class MinHeap<T> {
     this.heap.push(value);
     this.heapifyUp();
   }
+
+  /**
+   * 힙에서 최소값을 제거하고 반환합니다.
+   * @returns {T | null} - 힙에서 제거된 최소값 또는 null (힙이 비어 있는 경우).
+   */
+  poll(): T | null {
+    if (this.heap.length === 0) {
+      return null;
+    }
+
+    if (this.heap.length === 1) {
+      return this.heap.pop()!;
+    }
+
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop()!;
+    this.heapifyDown();
+
+    return min;
+  }
 }

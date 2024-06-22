c156bcd9285a45bf3d81c548f02416c8fe5bb08b
jaceleedev <jaceleedev@gmail.com>
2024-06-22 17:17:40 +0900

    feat: :sparkles: implement deleteAt in MaxHeap

--- src/data-structures/tree/heap/MaxHeap.ts
+++ src/data-structures/tree/heap/MaxHeap.ts
@@ -33,4 +33,17 @@ export class MaxHeap<T> {
       this.heapifyDown(index);
     }
   }
+
+  /**
+   * 특정 인덱스의 요소를 삭제합니다.
+   * @param {number} index - 삭제할 요소의 인덱스.
+   */
+  deleteAt(index: number): void {
+    if (index < 0 || index >= this.heap.length) {
+      throw new Error('Index out of bounds');
+    }
+
+    this.heap[index] = this.heap.pop()!;
+    this.heapifyDown(index);
+  }
 }

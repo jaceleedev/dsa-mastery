b0b9ec884c578ee7781f055f5893cdfe38ca422a
jaceleedev <jaceleedev@gmail.com>
2024-06-22 16:00:07 +0900

    feat: :sparkles: implement insert in MaxHeap

--- src/data-structures/tree/heap/MaxHeap.ts
+++ src/data-structures/tree/heap/MaxHeap.ts
@@ -4,4 +4,13 @@ export class MaxHeap<T> {
   constructor() {
     this.heap = [];
   }
+
+  /**
+   * 힙에 새로운 값을 삽입합니다.
+   * @param {T} value - 삽입할 값.
+   */
+  insert(value: T): void {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
 }

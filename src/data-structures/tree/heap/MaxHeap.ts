d3c55597df51f799648a7d8f2c2a9188a8de72a2
jaceleedev <jaceleedev@gmail.com>
2024-06-22 16:05:35 +0900

    feat: :sparkles: implement updateAt in MaxHeap

--- src/data-structures/tree/heap/MaxHeap.ts
+++ src/data-structures/tree/heap/MaxHeap.ts
@@ -13,4 +13,24 @@ export class MaxHeap<T> {
     this.heap.push(value);
     this.heapifyUp();
   }
+
+  /**
+   * 특정 위치의 값을 업데이트하고 힙 속성을 유지합니다.
+   * @param {number} index - 값을 업데이트할 요소의 인덱스.
+   * @param {T} value - 업데이트할 값.
+   */
+  updateAt(index: number, value: T): void {
+    if (index < 0 || index >= this.heap.length) {
+      throw new Error('Index out of bounds');
+    }
+
+    const oldValue = this.heap[index];
+    this.heap[index] = value;
+
+    if (value > oldValue) {
+      this.heapifyUp(index);
+    } else {
+      this.heapifyDown(index);
+    }
+  }
 }

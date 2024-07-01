55721a76ee3aa87460c34d77c266d4d0ae0129e5
jaceleedev <jaceleedev@gmail.com>
2024-07-01 14:45:17 +0900

    feat: :sparkles: implement addVertex in UndirectedGraph using adjacencyList

--- src/data-structures/graph/adjacency-list/UndirectedGraph.js
+++ src/data-structures/graph/adjacency-list/UndirectedGraph.js
@@ -2,4 +2,17 @@ export class UndirectedGraph {
   constructor() {
     this.adjacencyList = {};
   }
+
+  // 생성 (Creation) 관련 메서드
+  // =========================
+
+  /**
+   * 그래프에 새로운 정점을 추가합니다.
+   * @param {string} vertex 추가할 정점
+   */
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
 }

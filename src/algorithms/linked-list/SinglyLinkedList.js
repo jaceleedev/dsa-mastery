a342edb6dfc0ac825ce514fc1e4c7561e8177761
jaceleedev <jaceleedev@gmail.com>
2024-07-02 13:58:03 +0900

    feat: :sparkles: implement findMiddleNode for SinglyLinkedList

--- src/algorithms/linked-list/SinglyLinkedList.js
+++ src/algorithms/linked-list/SinglyLinkedList.js
@@ -21,3 +21,20 @@ function reverseLinkedList(list) {
 
   return list;
 }
+
+/**
+ * 리스트의 중간 노드 값을 반환합니다.
+ * @param {SinglyLinkedList} list - 중간 노드를 찾을 리스트
+ * @returns {Node} 중간 노드 값
+ */
+function findMiddleNode(list) {
+  let slow = list.head;
+  let fast = list.head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow.value;
+}
